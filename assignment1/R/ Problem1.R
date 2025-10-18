31458e379924312973c0918c305b53c9e54fa327
minjoo0760 <kmj31202@gmail.com>
2025-10-18 18:34:04 +0900

    과제1_problem1 : r코드의 경로 수정

--- assignment1/R/ Problem1.R
+++ assignment1/R/ Problem1.R
@@ -14,4 +14,13 @@ write.csv(data1, "./assignment1/data/problem1_data.csv")
 
 # (b) 저장한 데이터 불러오고, ggplot2를 바탕으로 산점도와 회귀곡선 적합을시각화
 #     (ggplot2의goem_smooth()에서 적절한 method 선택)
-read.csv("./assignment1/data/problem1_data.csv")
+data2 <- read.csv("./assignment1/data/problem1_data.csv")
+
+library(ggplot2)
+plot1<- ggplot(data=data2, aes(x=x, y=y))+
+  geom_point(size=2)+
+  geom_smooth(color='red',size=2)
+plot1
+
+# (c)  ggsave() 함수를 이용하여 작성한 플랏을 plots/폴더에 저장하는 코드를 작성
+ggsave(path = "./assignment1/plots", filename = "problem1_plot.png")
